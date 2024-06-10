1b70bbf26a9c06b772b43b10d7708440376c7165
ghimireaacs <aacs.csu@gmail.com>
2024-06-10 23:56:17 +1000

    learning Routers modified

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,27 @@
 
 import './App.css'
 
-
+import {Route, Routes} from "react-router-dom"
 
 import Leagues from './Pages/Leagues/Leaguespage'
 import Navmenu from './components/Navbar/Navbar'
+import About from './Pages/About'
 
 function App() {
 
 
   return (
     <>
-
+    <div className=' flex-auto w-auto'>
       <Navmenu />
-      <div className=' flex-auto w-auto'>
-      <Leagues />
-      <p>Hello</p>
-      </div>
+      <Routes>
+        
+        <Route path='/' element={<Leagues />} />
+        <Route path='/about' element={<About />} />
+
       
+      </Routes>
+      </div>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter, Routes } from 'react-router-dom'
-import Routing from './components/Routes/Routing.tsx'
-import Navmenu from './components/Navbar/Navbar.tsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Routing />
+    <BrowserRouter>    
+      
+      <App />
     
-    {/* <App /> */}
     </BrowserRouter>
   </React.StrictMode>,
 )
