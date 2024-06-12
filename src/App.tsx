d1d2b1fb7f50a020392ff283904753c7bf9e5652
ghimireaacs <aacs.csu@gmail.com>
2024-06-12 23:38:18 +1000

    modifying routes

--- src/App.tsx
+++ src/App.tsx
@@ -15,14 +15,15 @@ function App() {
     <>
     <div className=' flex-auto w-auto'>
       <Navmenu />
+
       <Routes>
         
         <Route path='/' element={<Leagues />} />
         <Route path='/about' element={<About />} />
         <Route path='/leagues' element={<Leagues />} />
-        <Route path='/leagues/:id' element={<Leagues />} />
+        <Route path='/epl' element={epl} />
+        {/* <Route path='/leagues/:id' element={<Leagues />} /> */}
 
-      
       </Routes>
       </div>
     </>
